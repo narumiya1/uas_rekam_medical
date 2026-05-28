7724288e34293186506f3e9e7d60980908ff2a81
narumiya1 <narumiiyamei@gmail.com>
2026-05-28 14:38:55 +0700

    sri_sumarni

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -11,43 +11,57 @@ class HomeView extends GetView<HomeController> {
     return Scaffold(
       appBar: AppBar(
         title: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Icon(Icons.local_hospital_rounded, size: 30),
-                const Text("SIM Rekam Medis", style: TextStyle(fontSize: 16)),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text(
-                        "Sri Sumarni \t\t",
-                        style: TextStyle(
-                          fontSize: 16,
-                        ),
-                      ),
-                    ],
+                const Text(
+                  textAlign: TextAlign.center,
+                  "Sistem Informasi Rekam Medis",
+                  style: TextStyle(
+                    fontSize: 16,
                   ),
-                )
+                ),
+                SizedBox(width: 10),
+                Icon(Icons.local_hospital_rounded, size: 30),
               ],
             ),
           ],
         ),
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: [
-          menu(title: "Pasien", icon: Icons.people, route: "/pasien"),
-          menu(title: "Dokter", icon: Icons.medical_services, route: "/dokter"),
-          menu(title: "Obat", icon: Icons.medication, route: "/obat"),
-          menu(title: "Poli", icon: Icons.local_hospital, route: "/poli"),
-          menu(
-              title: "Pendaftaran",
-              icon: Icons.app_registration,
-              route: "/pendaftaran"),
-          menu(title: "Laporan", icon: Icons.print, route: "/laporan"),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(14),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: [
+            menu(title: "Pasien", icon: Icons.people, route: "/pasien"),
+            menu(
+                title: "Dokter",
+                icon: Icons.medical_services,
+                route: "/dokter"),
+            menu(title: "Obat", icon: Icons.medication, route: "/obat"),
+            menu(title: "Poli", icon: Icons.local_hospital, route: "/poli"),
+            menu(title: "Pendaftaran", icon: Icons.edit, route: "/pendaftaran"),
+            menu(title: "Laporan", icon: Icons.print, route: "/laporan"),
+          ],
+        ),
+      ),
+      // BAGIAN BAWAH
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(
+          vertical: 12,
+        ),
+        child: const Text(
+          // " © Sri Sumarni - 25403024 - MIK-L41/25",
+          "Devina Qurrota ‘Aini Setiawan - 25403043 -\nMIK-EL41/25",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
@@ -62,7 +76,7 @@ class HomeView extends GetView<HomeController> {
         Get.toNamed(route);
       },
       child: Card(
-        elevation: 5,
+        elevation: 6,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
